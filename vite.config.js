d0262dfa26d0a22812f19c9a7e3287e2ec62275c
noeltesaluna <ntesaluna03@gmail.com>
2025-02-09 10:38:36 +1100

    implemented docker

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react(), tsconfigPaths()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
 })
